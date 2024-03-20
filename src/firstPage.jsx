db7f1356f3b1b747ad95d57cbfd53306c78db242
Kafeel Kazeem <kafeelkazeem@gmail.com>
2024-03-20 16:26:38 +0100

    made changes

---
+++ src/firstPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FirstPage = () => {
+    const style = {
+        color: '#fff',
+        fontSize: '30px'
+    }
+  return (
+    <h1 style={style}>Hi welcome to my website thanks for checking in</h1>
+  )
+}
+
+export default FirstPage

282fba90e87dd69c50c98b3dbaa798751ef4b33f
Kafeel Kazeem <kafeelkazeem@gmail.com>
2024-03-22 14:12:10 +0100

    new changes

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,71 +1,73 @@
-import React from 'react'
-import { Box, Toolbar, Typography } from '@mui/material'
-import ResponsiveDrawer from '../component/Drawer'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import { Grid, Paper, Typography } from '@mui/material';
+import { Toolbar, Box } from '@mui/material';
+import ResponsiveDrawer from '../component/Drawer';
+import { red, purple, teal, orange, blueGrey, lightBlue, indigo } from '@mui/material/colors';
 
-// const Content = () =>{
-//     return(
-//         <>
-//         <Typography paragraph>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-//           tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-//           enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-//           imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-//           Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-//           Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-//           adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-//           nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-//           leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-//           feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-//           consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-//           sapien faucibus et molestie ac.
-//         </Typography>
-//         <Typography paragraph>
-//           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-//           eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-//           neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-//           tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-//           sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-//           tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-//           gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-//           et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-//           tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-//           eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-//           posuere sollicitudin aliquam ultrices sagittis orci a.
-//         </Typography>
-//         </>
-//     )
-// }
+const Root = styled('div')(({ theme }) => ({
+  flexFlow: 1,
+  padding: theme.spacing(2),
+}))
+
+ const Item = styled(Paper)(({ theme }) => ({
+     padding: theme.spacing(4),
+     textAlign: 'center',
+     color: theme.palette.text.secondary,
+     height: '200px', // Increase the height
+  }))
+
+const Dashboard = () => {
+  // Mock data (replace with actual data from API or state)
+  const totalStudents = 500;
+  const paidStudents = 400;
+  const notPaidStudents = 50;
+  const owingStudents = 50;
+
+  const color = red[500];
+
+  return (
+    <Root>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} md={6} lg={6}> {/* Modify grid size for large screens */}
+          <Item sx={{backgroundColor: purple[400], color: 'primary.contrastText' }}>
+            <Typography variant="h5" sx={{textAlign: 'left'}}>Total Students</Typography>
+            <Typography variant="h4" sx={{textAlign: 'right', paddingTop: '3rem'}}>{totalStudents}</Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={6}> {/* Modify grid size for large screens */}
+          <Item sx={{backgroundColor: teal[400], color: 'primary.contrastText'}}>
+            <Typography variant="h5" sx={{textAlign: 'left'}}>Students Paid</Typography>
+            <Typography variant="h4" sx={{textAlign: 'right', paddingTop: '3rem'}}>{paidStudents}</Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={6}> {/* Modify grid size for large screens */}
+          <Item sx={{backgroundColor: blueGrey[400], color: 'primary.contrastText'}}>
+            <Typography variant="h5" sx={{textAlign: 'left'}}>Students Not Paid</Typography>
+            <Typography variant="h4" sx={{textAlign: 'right', paddingTop: '3rem'}}>{notPaidStudents}</Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={6}> {/* Modify grid size for large screens */}
+          <Item sx={{backgroundColor: indigo[400], color: 'primary.contrastText'}}>
+            <Typography variant="h5" sx={{textAlign: 'left'}}>Students Owing</Typography>
+            <Typography variant="h4" sx={{textAlign: 'right', paddingTop: '3rem'}}>{owingStudents}</Typography>
+          </Item>
+        </Grid>
+      </Grid>
+    </Root>
+  );
+};
 
 const Dash_board = () => {
   return (
     <Box sx={{ display: 'flex' }}>
-        <ResponsiveDrawer />
-         <Box
-        component="main"
-         sx={{ flexGrow: 1, p: 3 }}
-      >
+      <ResponsiveDrawer />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-        <Typography paragraph>
-          Hi welcome to my project. My name is Kafeel Kazeem, this is just a practice project built with react and material ui, the app is deployed
-          on vercel. It is meant to be a student management system which will enable the admin to keep track of student they have in their
-          school.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Dashboard />
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Dash_board
+export default Dash_board;
